6a2328906f9de2437c583aa1c8ef5e3c55844c4e
Eli Bell <elibell@elibell.net>
2024-04-25 23:25:57 -0400

    cpp lsp

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -15,9 +15,7 @@ return {
 			lspconfig.pyright.setup({})
 			lspconfig.lua_ls.setup({})
 			lspconfig.texlab.setup({})
-			lspconfig.clangd.setup({
-				cmd = { "clangd-mp-11" }
-			})
+			lspconfig.clangd.setup({ cmd = { "clangd-mp-11" } })
 			---- Global mappings.
 			---- See `:help vim.diagnostic.*` for documentation on any of the below functions
 			--vim.keymap.set('n', '<space>dd', vim.diagnostic.open_float)
@@ -27,33 +25,40 @@ return {
 			--
 			---- Use LspAttach autocommand to only map the following keys
 			---- after the language server attaches to the current buffer
-			--vim.api.nvim_create_autocmd('LspAttach', {
-			--group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-			--callback = function(ev)
-			---- Enable completion triggered by <c-x><c-o>
-			--vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-			--
-			---- Buffer local mappings.
-			---- See `:help vim.lsp.*` for documentation on any of the below functions
-			--local opts = { buffer = ev.buf }
-			----vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-			----vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-			----vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-			----vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-			----vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-			----vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-			----vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-			----vim.keymap.set('n', '<space>wl', function()
-			--print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-			--end, opts)
-			--vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-			--vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-			--vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-			--vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-			--vim.keymap.set('n', '<space>m', function()
-			--vim.lsp.buf.format { async = true }
-			--end, opts)
-			--end
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+				callback = function(ev)
+					local map = vim.g.map
+					---- Enable completion triggered by <c-x><c-o>
+					--vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+					--
+					-- Buffer local mappings.
+					-- See `:help vim.lsp.*` for documentation on any of the below functions
+
+					local map = vim.g.map
+
+					-- Buffer local mappings.
+					-- See `:help vim.lsp.*` for documentation on any of the below functions
+
+					local opts = { buffer = ev.buf }
+
+					map("n", "gD", vim.lsp.buf.declaration, "Go to declaration of symbol under cursor", opts)
+					map("n", "gd", vim.lsp.buf.definition, "Go to definition of symbol under cursor", opts)
+					map("n", "<C-k>", vim.lsp.buf.signature_help, "Show signature help for function call under cursor", opts)
+					map("n", "<space>wa", vim.lsp.buf.add_workspace_folder, "Add current workspace folder to workspace", opts)
+					map("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, "Remove current workspace folder from workspace", opts)
+					map("n", "<space>wl", function()
+						print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+					end, "List all workspace folders in current workspace", opts)
+					map("n", "<space>D", vim.lsp.buf.type_definition, "Go to type definition of symbol under cursor", opts)
+					map("n", "<space>rn", vim.lsp.buf.rename, "Rename symbol under cursor", opts)
+					map("nv", "<space>ca", vim.lsp.buf.code_action, "Get code actions for symbol under cursor", opts)
+					map("n", "gr", vim.lsp.buf.references, "Get references to symbol under cursor", opts)
+					map("n", "<space>m", function()
+						vim.lsp.buf.format { async = true }
+					end, "Format current buffer", opts)
+				end,
+			})
 		end,
 	},
 }
