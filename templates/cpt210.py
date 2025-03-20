1c9d7494c294925951d70e8b6060b3bb2275d9e9
elibell <elibell@elibell.net>
2025-03-19 22:47:49 -0400

    update labs template

--- templates/cpt210.py
+++ templates/cpt210.py
@@ -8,11 +8,10 @@
 #
 #     DESCRIPTION:
 #
-#       This Python script is intended for a Raspberry Pi 5, running Raspberry Pi OS
-#       (bookworm) and requires the gpio_manager module, which replaces the obsolete
-#       sysfs interface. The peripheral circuitry consists of
-#       ... 
-#       Ω
+#       This Python script is intended for a Raspberry Pi 5, running Raspberry
+#       Pi OS (bookworm) and requires the gpio_manager module, which replaces
+#       the obsolete sysfs interface. The peripheral circuitry consists of ...
+#       Ω resistor
 #
 # *****************************************************************************
 
@@ -20,28 +19,20 @@
 # Python Modules
 # ---------------------------------------------------
 import gpio_manager as GPIO
-import time
 
 # ---------------------------------------------------
 # Constants
 # ---------------------------------------------------
 
-# GPIO Pin States
-BUTTON_PRESSED = GPIO.PinState.LOW
-LED_ON = GPIO.PinState.HIGH
-LED_OFF = GPIO.PinState.LOW
-
 # GPIO number based on BCM GPIO numbering scheme
 BUTTON_PIN = 18
 LED_PIN = 26
 
-# Time Delays
-READ_DELAY_200MS = 0.2
-READ_DELAY_500MS = 0.5
-READ_DELAY_1SEC = 1
-
 # -----------------------------------------------------------------------------
-# DESCRIPTION
+# FUNCTION NAME:
+#   setup_gpio()
+#
+# DESCRIPTION: 
 #   Configures GPIO ... 
 #
 # INPUT PARAMETERS:
@@ -53,31 +44,36 @@ READ_DELAY_1SEC = 1
 # RETURN:
 #   GPIO interface object handle of the gpio_manager module.
 # -----------------------------------------------------------------------------
-# -----------------------------------------------------------------------------
 def setup_gpio():
     gpio_handle = GPIO.GPIOManager()
 
-    gpio_handle.add_input_pin(BUTTON_PIN, GPIO.InternPullResistorState.PULLUP)
+    # https://gpio-manager.readthedocs.io/en/latest/index.html 
 
-    gpio_handle.add_output_pin(LED_PIN, pin_state=LED_OFF)
-
-    gpio_handle.assign_callback(
-        pin_num=BUTTON_PIN,
-        callback=print,
-        trigger_edge=GPIO.TriggerEdge.RISING,
-        debounce_time_ms=20,
-    )
-    
     return gpio_handle
 
 
 # -----------------------------------------------------------------------------
-#  main() function
+# FUNCTION NAME:
+#   main()
+#
+# DESCRIPTION: 
+#   Main Method: configures GPIO and ...
+#
+# INPUT PARAMETERS:
+#   none
+#
+# OUTPUT PARAMETERS:
+#   none
+#
+# RETURN:
+#   none
 # -----------------------------------------------------------------------------
 def main():
 
+    gpio_handle = setup_gpio()
+
     try:
-        gpio_handle = setup_gpio()
+        pass
 
 
     except KeyboardInterrupt:
@@ -88,7 +84,7 @@ def main():
         print("\nGPIO Ports have been cleaned up.")
 
 
-# -----------------------------------------------------------------------------
+# *****************************************************************************
 if __name__ == "__main__":
     print("\n********************** PROGRAM IS RUNNING **********************")
     print(f"********************** {'[:VIM_EVAL:]expand("%p")[:END_EVAL:]':^18} **********************\n")
